0cde9dd26e4a8f20d8ca336eeb57014f74771302
SemicolonNotFound <lucash.langp@outlook.com>
2020-02-26 17:56:11 -0300

    little fix

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -3,12 +3,14 @@ const Dev = require('../models/Dev');
 const parseStringAsArray = require('../utils/parseStringAsArray');
 
 module.exports = {
+    // listar.
     async index(request, response) {
         const devs = await Dev.find();
 
         return response.json(devs);
     },
 
+    //
     async store(request, response) {
         const { github_username, techs, latitude, longitude } = request.body;
 
